c284b83f55397d0db48639f8b76422f4a4be5d7b
luisalexis2021 <119871038+motkvil@users.noreply.github.com>
2022-12-31 07:20:17 -0500

    requirements.txt

--- backendMotkvil/settings.py
+++ backendMotkvil/settings.py
@@ -195,3 +195,15 @@ CORS_ALLOWED_ORIGINS = [
     "https://motk.onrender.com",
     "http://localhost:3000",
 ]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
