8e18b47e56fdbb196ea0a40a3fffa219cb83b601
luisalexis2021 <119871038+motkvil@users.noreply.github.com>
2023-01-03 05:37:00 -0500

    Update settings.py

--- backendMotkvil/settings.py
+++ backendMotkvil/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default = 'motk')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['https://backendmotk.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 
